74e8c7d8f14fbb79f2d3b60a459d3c2faafe56c5
leptyagin <leptyagin.ser@gmail.com>
2024-02-09 02:54:35 +1000

    6 lesson

--- app/Http/Controllers/Admin/Category/IndexController.php
+++ app/Http/Controllers/Admin/Category/IndexController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Admin\Main;
+namespace App\Http\Controllers\Admin\Category;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
@@ -9,6 +9,6 @@ class IndexController extends Controller
 {
     public function __invoke()
     {
-        return view('admin.main.index');
+        return view('admin.categories.index');
     }
 }
---
+++ resources/views/admin/includes/sidebar.blade.php
@@ -0,0 +1,20 @@
+<!-- Main Sidebar Container -->
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="index3.html" class="brand-link">
+        <span class="brand-text font-weight-light">Lara Blog</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+        <ul class="pt-3 nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="nav-icon fas fa-tree"></i>
+                    <p>Категории</p>
+                </a>
+            </li>
+        </ul>
+    </div>
+    <!-- /.sidebar -->
+</aside>
