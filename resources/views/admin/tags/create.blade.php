18aff6f7192706ccac25a8ee82a5cd642fbc2a6a
leptyagin <leptyagin.ser@gmail.com>
2024-02-10 02:50:29 +1000

    14 lesson

---
+++ resources/views/admin/tags/create.blade.php
@@ -0,0 +1,39 @@
+@extends('admin.layouts.main')
+@section('content')
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">Добавление категории</h1>
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content-header -->
+
+        <!-- Main content -->
+        <section class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <form action="{{ route('admin.tag.store') }}" method="POST" class="col-4">
+                        @csrf
+                        <div class="form-group">
+                            <label>Название тэга</label>
+                            <input type="text" class="form-control" name="title" placeholder="Название тэга">
+
+                            @error('title')
+                                <div class="text-danger">Это поле необходимо заполнить</div>
+                            @enderror()
+                        </div>
+                        <input type="submit" class="btn btn-primary" value="Добавить">
+                    </form>
+                </div>
+            </div><!--/. container-fluid -->
+        </section>
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+@endsection
